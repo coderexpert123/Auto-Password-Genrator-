849833aee9c031815f32f8a877feb21b01ada0ca
Nishant <nishantsingh2jan1998@gmail.com>
2020-05-12 10:00:25 +0530

    Password Auto Generator

---
+++ main.py
@@ -0,0 +1,18 @@
+import string
+import  random
+if __name__=="__main__":
+    s1=string.ascii_uppercase
+    s2=string.ascii_lowercase
+    s3=string.digits
+    s4=string.punctuation
+    plen=int(input("Enter the Password Length :  "))
+    s=[]
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+
+    random.shuffle(s)
+    print("Your Passwrd is :")
+    print("".join(random.sample(s,plen)))
+
